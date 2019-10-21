552436ecaafd3d7b4261b3e20a0426806e330886
max-jellycat <max@jellycat.fr>
2019-10-21 03:02:45 +0200

    changed eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   rules: {
     'no-confusing-arrow': 0,
     'no-unused-expressions': 0,
+    'no-unused-vars': 1,
     'max-len': 0,
     'jsx-a11y/label-has-associated-control': 0,
   },
